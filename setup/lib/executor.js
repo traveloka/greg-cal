e6988079cabba6c7e901c5ca3c048296528c09db
Shandy Pranata Darmawan <38228367+shandypd@users.noreply.github.com>
2020-01-20 10:20:33 +0700

    Setup

--- setup/lib/executor.js
+++
@@ -1,9 +0,0 @@
-const { execSync } = require('child_process');
-
-module.exports = function (workingDirectory) {
-    return {
-        exec: function (command) {
-            execSync(command, { cwd: workingDirectory  ,stdio: 'inherit' });
-        }
-    }
-}
